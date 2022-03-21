f8ddf12ef6f29e90f592c57d417bbaa88e1481bf
Oscar Umaña <oscarumana1021@gmail.com>
2022-03-21 10:27:04 -0600

    update

---
+++ house-marketplace/src/components/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import useAuthStatus from "../hooks/useAuthStatus";
+import { Navigate, Outlet } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+
+const PrivateRoute = () => {
+  const {loggedIn, checkingStatus} = useAuthStatus()
+
+  if(checkingStatus){
+      return <h3>Loading...</h3>
+  }
+
+  return loggedIn ? <Outlet /> : <SignIn />;
+};
+
+export default PrivateRoute;
